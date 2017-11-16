fa4ba4d3e09151dfdee4e786fac2f144f3d3781e
StephHazlitt <stephhazlitt@gmail.com>
2017-11-15 21:40:24 -0800

    clean response data

--- clean.R
+++ clean.R
@@ -4,3 +4,15 @@ library(dplyr) # data munging
 
 ## Load raw data if not already in local repository
 if (!exists("state.responses")) load("tmp/raw_data.RData")
+
+## state responses
+state.responses <- state.responses.raw %>% 
+  select(-blank) 
+
+## div responses
+div.responses <- div.responses.raw %>% 
+  select(-blank) %>% 
+  left_join(state.div) %>%
+  filter(!is.na(div) & div != "Australia") %>%
+  filter(!grepl(" Divisions$", div))
+  
