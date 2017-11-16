44a29c8642b6354d09c6b0fdff7edc008a0a6791
StephHazlitt <stephhazlitt@gmail.com>
2017-11-15 22:09:44 -0800

    percent response vs yes by div & state

--- plots.R
+++ plots.R
@@ -1,1 +1,23 @@
 ## Dive into the tidy data
+
+library(ggplot2) # plotting
+
+## Load clean data if not already in local repository
+if (!exists("state.responses")) load("tmp/clean_data.RData")
+
+(div.resp.plot <- div.responses %>% 
+  filter(!grepl(" Total)$", div)) %>% 
+  filter(div != "Total") %>% 
+  ggplot(aes(x = clear_response_percent, 
+                  y = clear_yes_percent)) + 
+  geom_point() +
+  facet_wrap(~state, nrow = 2)
+)
+ 
+(tot.resp.plot <- div.responses %>% 
+    filter(grepl("Total", div)) %>% 
+    filter(div != "Total") %>% 
+    ggplot(aes(x = clear_response_percent, 
+               y = clear_yes_percent)) + 
+    geom_point()
+)
