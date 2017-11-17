62f8fd5da10f6be908f16cf9c5850a670cad31dd
StephHazlitt <stephhazlitt@gmail.com>
2017-11-16 22:28:28 -0800

    tweak clean

--- clean.R
+++ clean.R
@@ -3,19 +3,21 @@
 library(dplyr) # data munging
 
 ## Load raw data if not already in local repository
-if (!exists("state.responses.raw")) load("tmp/raw_data.RData")
+if (!exists("responses.raw")) load("tmp/raw_data.RData")
 
-## state responses
-state.responses <- state.responses.raw %>% 
-  select(-blank) 
 
-## div responses
-div.responses <- div.responses.raw %>% 
+## responses
+responses.raw$div[responses.raw$div == "Total"] <- "Australia"
+
+responses <- responses.raw %>% 
   select(-blank) %>% 
   left_join(state.div) %>%
-  filter(!is.na(div) & div != "Australia") %>%
-  filter(!grepl(" Divisions$", div))
+  filter(!is.na(div) & !is.na(clear_yes_count)) %>%
+  filter(!grepl(" Divisions$", div)) %>% 
+  mutate(state = replace(state, is.na(state), "Australia"))
+
+## participation
+#participation <- participation.raw %>% 
   
 ## Save clean data in tmp folder
-save(state.responses, div.responses,
-     file = "tmp/clean_data.RData")
+save(responses, file = "tmp/clean_data.RData")
